e253c7623dda40a0db920a4a2e7f97d1067f13fa
毛小喵 <plecun@users.noreply.github.com>
2021-09-04 11:23:01 +0800

    Create App.vue

---
+++ cs/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch('getUserInformation')
+  },
+}
+</script>
+
+<style>
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  min-width: 1200px;
+}
+</style>
